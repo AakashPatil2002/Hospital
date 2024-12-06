096378f2fb2a07cac10928d381eba7e077511acf
AakashPatil2002 <129407059+AakashPatil2002@users.noreply.github.com>
2024-12-06 09:56:37 +0530

    uploaded

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+$(".about-clinic-carousel").owlCarousel({
+    items:1,
+    autoplay:true,
+    autoplayTimeout:5000,
+    dots:false,
+    nav:true,
+    loop:true,
+    navText:["<i class='bi bi-arrow-left-short'></i>","<i class='bi bi-arrow-right-short'></i>"]
+});
+AOS.init();
